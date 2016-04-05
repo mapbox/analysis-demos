4237dd03d09083c299eadac9424b95020c3f1e34
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-04-05 09:11:54 -0400

    Add address to denue tiles and translate column names to english

--- denue/data.sh
+++ denue/data.sh
@@ -8,6 +8,6 @@ cd data
 wget -O - http://www3.inegi.org.mx/sistemas/descarga/descargaarchivo.aspx?file=DENUE%2fEntidad_federativa%2f09_Distrito_Federal%2fdenue_09_25022015_shp.zip > denue_09_25022015.zip
 unzip denue_09_25022015.zip
 echo "Transforming data to vector tilesets"
-ogr2ogr -f GeoJSON denue_09_25022015.json DENUE_INEGI_09_.shp -select nom_estab,codigo_act,latitud,longitud -progress
+ogr2ogr -f GeoJSON denue_09_25022015.json DENUE_INEGI_09_.shp -sql "SELECT nom_estab AS name, codigo_act AS activity, nom_vial+' '+numero_ext AS address, latitud, longitud FROM DENUE_INEGI_09_" -progress
 tippecanoe -o denue_09_25022015.mbtiles denue_09_25022015.json
 cd ..
